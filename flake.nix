449f0745d934d8ef9873b38076d74adc5b2b5502
LeshaInc <includeurl@gmail.com>
2023-06-19 21:45:58 +0300

    Add bevy engine

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs = { self, nixpkgs, utils, fenix }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        rust-toolchain = with fenix.packages.${system};
+          combine (with complete; [
+            rustc
+            rust-src
+            cargo
+            clippy
+            rustfmt
+            rust-analyzer
+          ]);
+      in
+      {
+        devShell = with pkgs; mkShell rec {
+          buildInputs = [
+            alsa-lib
+            cargo-criterion
+            gnuplot
+            lld
+            mdbook
+            pkg-config
+            rnix-lsp
+            rust-toolchain
+            udev
+            vulkan-loader
+            vulkan-validation-layers
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+          ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d/";
+
+          RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
+          RUST_LOG = "warn";
+          RUST_BACKTRACE = 1;
+        };
+      }
+    );
+}

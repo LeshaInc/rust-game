e6085d18c4c9349e9a97a3af97442b7f6c645acb
LeshaInc <includeurl@gmail.com>
2023-06-23 22:41:31 +0300

    Hardcode dither matrix inside shader

--- assets/shaders/pixel.wgsl
+++ assets/shaders/pixel.wgsl
@@ -12,9 +12,6 @@ struct PixelMaterial {
 @group(1) @binding(0)
 var<uniform> material: PixelMaterial;
 
-@group(1) @binding(1)
-var dither_matrix: texture_2d<f32>;
-
 fn single_light(
     frag_coord: vec2<f32>,
     mesh_normal: vec3<f32>,
@@ -28,7 +25,14 @@ fn single_light(
     light *= f32(material.bands);
 
 #ifdef DITHER_ENABLED
-    let bayer = pow(textureLoad(dither_matrix, (vec2<u32>(frag_coord) + material.dither_offset) % 4u, 0).r, 1.0 / 2.2);
+    var dither_matrix = mat4x4<f32>(
+        vec4<f32>( 0.0 / 16.0, 12.0 / 16.0,  3.0 / 16.0, 15.0 / 16.0),
+        vec4<f32>( 8.0 / 16.0,  4.0 / 16.0, 11.0 / 16.0,  7.0 / 16.0),
+        vec4<f32>( 2.0 / 16.0, 14.0 / 16.0,  1.0 / 16.0, 13.0 / 16.0),
+        vec4<f32>(10.0 / 16.0,  6.0 / 16.0,  9.0 / 16.0,  5.0 / 16.0)
+    );
+    let idx = (vec2<u32>(frag_coord) + material.dither_offset) % 4u;
+    let bayer = dither_matrix[idx.x][idx.y];
     light = mix(floor(light), ceil(light), f32(fract(light) > bayer));
 #elseif
     light = round(light);
--- crates/rg_pixel_material/src/lib.rs
+++ crates/rg_pixel_material/src/lib.rs
@@ -28,8 +28,6 @@ pub struct PixelMaterial {
     // TODO: shader globals
     #[uniform(0)]
     pub dither_offset: UVec2,
-    #[texture(1)]
-    pub dither_matrix: Option<Handle<Image>>,
 }
 
 impl Default for PixelMaterial {
@@ -38,7 +36,6 @@ impl Default for PixelMaterial {
             color: Color::WHITE,
             bands: 4,
             dither_enabled: true,
-            dither_matrix: None,
             dither_offset: UVec2::ZERO,
         }
     }
@@ -82,27 +79,19 @@ impl From<&PixelMaterial> for PixelMaterialKey {
     }
 }
 
-#[derive(Debug, Resource)]
-pub struct GlobalDitherMatrix(pub Handle<Image>);
-
 #[derive(Debug, Default, Resource)]
 pub struct GlobalDitherOffset(pub UVec2);
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands) {
     let dither_offset = GlobalDitherOffset::default();
     commands.insert_resource(dither_offset);
-
-    let dither_matrix = GlobalDitherMatrix(asset_server.load("images/bayer4x4.png"));
-    commands.insert_resource(dither_matrix);
 }
 
 fn update_globals(
     mut materials: ResMut<Assets<PixelMaterial>>,
     dither_offset: Res<GlobalDitherOffset>,
-    dither_matrix: Res<GlobalDitherMatrix>,
 ) {
     for (_, material) in materials.iter_mut() {
         material.dither_offset = dither_offset.0;
-        material.dither_matrix = Some(dither_matrix.0.clone());
     }
 }
